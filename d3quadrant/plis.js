03fa86d5702e3dcdcc5066722ef2792047fd5681
phollott <piers.hollott@gmail.com>
2015-02-14 12:21:01 -0800

    Create plis.js

---
+++ d3quadrant/plis.js
@@ -0,0 +1,67 @@
+var items = [{
+        "name": "Phase 2",
+            "category": "maintenance",
+            "price": 3,
+            "rating": 9,
+            "size": 6
+    }, {
+        "name": "PLIS-HIAL Separation",
+            "category": "maintenance",
+            "price": 4,
+            "rating": 7,
+            "size": 6
+    }, {
+        "name": "Phase 3",
+            "category": "change",
+            "price": 9,
+            "rating": 8,
+            "size": 6
+    }, {
+        "name": "EMR",
+            "category": "change",
+            "price": 7,
+            "rating": 6,
+            "size": 6
+    }, {
+        "name": "Orders System",
+            "category": "change",
+            "price": 8,
+            "rating": 7,
+            "size": 10
+    }, {
+        "name": "Cache Refactor",
+            "category": "maintenance",
+            "price": 8,
+            "rating": 1,
+            "size": 8
+    }, {
+        "name": "SWT WebLogic",
+            "category": "change",
+            "price": 3,
+            "rating": 4,
+            "size": 4
+    }, {
+        "name": "SWT Spring 3",
+            "category": "maintenance",
+            "price": 1,
+            "rating": 3,
+            "size": 3
+    }, {
+        "name": "Legacy Data Support",
+            "category": "change",
+            "price": 2,
+            "rating": 6,
+            "size": 3
+    }, {
+        "name": "EMS-ORA",
+            "category": "maintenance",
+            "price": 4,
+            "rating": 2,
+            "size": 2
+    }, {
+        "name": "Component Sunset",
+            "category": "maintenance",
+            "price": 3,
+            "rating": 1,
+            "size": 4
+    }];
